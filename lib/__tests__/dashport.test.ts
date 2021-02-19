3219cb6cb6289e3fcfa5ff8ce03022cee49f7bbc
ed-deng <edeng2@binghamton.edu>
2021-02-19 16:46:04 -0500

    Dashport test.ts

--- lib/__tests__/dashport.test.ts
+++ lib/__tests__/dashport.test.ts
@@ -1,8 +1,8 @@
 import Dashport from '../dashport.ts';
-import { assertEquals, assertNotEquals, assertThrowsAsync } from '../../deps.ts';
+import { assertEquals, assertNotEquals, assertThrows, assertThrowsAsync } from '../../deps.ts';
 
 class TestStrat {
-  async router() {
+  async router(ctx: any, next: any) {
     return {
       userInfo: {
         provider: 'test',
@@ -17,14 +17,12 @@ class TestStrat {
   }
 }
 
-
-
-
 Deno.test({
   name: "A new Dashport instance should include properties _sId, initialize, authenticate, " +
   "addSerializer, removeSerializer, addStrategy, removeStrategy, and getUserInfo.",
   fn(): void {
-    const oakTestDash = new Dashport('oak')
+    const oakTestDash = new Dashport('oak');
+
     assertEquals(oakTestDash._sId, '');
     assertNotEquals(oakTestDash.initialize, undefined);
     assertNotEquals(oakTestDash.authenticate, undefined)
@@ -37,88 +35,128 @@ Deno.test({
 });
 
 Deno.test({
-  name: "initialize should be the correct server middleware with whichever framework Dashport is instantiated with",
+  name: "when Dashport is invoked with 'oak', initialize should create a _dashport object on ctx.state",
   fn(): void {
     const oakTestDash = new Dashport('oak');
-    
-    assertEquals(oakTestDash.initialize, async (ctx: ) => {
+    const fakeOakCtx = {
+      app: {},
+      cookies: {},
+      request: {},
+      respond: {},
+      response: {},
+      socket: {},
+      state: {},
+      assert: () => 1,
+      send: () => 2,
+      sendEvents: () => 3,
+      throw: () => 4,
+      upgrade: () => 5,
+      params: {}
+    };
+    const fakeNext = () => 1;
 
-    })
-
-
-  },
-});
-
-
-Deno.test({
-  name: "addStrategy method should add a strategy to _strategies",
-  fn(): void {
-    const oakTestDash = new Dashport('oak');
-    const fakeStrat = new TestStrat();
-
-    assertEquals(oakTestDash[_strategies], {});
-    oakTestDash.addStrategy('test', fakeStrat);
-    assertEquals(oakTestDash[_strategies], { test: fakeStrat });
-  },
-});
-
-Deno.test({
-  name: "removeStrategy method should remove a strategy on _strategies",
-  fn(): void {
-    const oakTestDash = new Dashport('oak');
-
-    assertEquals();
-    oakTestDash.removeStrategy('test');
-
-    assertEquals();
+    assertEquals(Object.keys(fakeOakCtx.state).length, 0);
+    oakTestDash.initialize(fakeOakCtx, fakeNext);
+    assertEquals(Object.keys(fakeOakCtx.state)[0], '_dashport');
   },
 });
 
 Deno.test({
-  name: "If a session object exists on ctx.state._dashport, authenticate method " + 
-        "should compare the IDs and invoke next",
+  name: "Authenticate method should check if strategy name passed in exists in _strategies",
   fn(): void {
     const oakTestDash = new Dashport('oak');
 
-    
-
-
+    oakTestDash.addStrategy('testStrat', new TestStrat());
+    assertThrows(() => oakTestDash.authenticate('hi'));
   },
 });
 
 Deno.test({
-  name: "If a session object does not exist on ctx.state._dashport, or IDs do not match, " + 
-        "authenticate method should begin the authentication process",
+  name: "If _dashport has not been initialized in state, authenticate method should throw an error",
   fn(): void {
     const oakTestDash = new Dashport('oak');
+    const fakeOakCtx = {
+      app: {},
+      cookies: {},
+      request: {},
+      respond: {},
+      response: {},
+      socket: {},
+      state: { _dashport: '' },
+      assert: () => 1,
+      send: () => 2,
+      sendEvents: () => 3,
+      throw: () => 4,
+      upgrade: () => 5,
+      params: {}
+    };
+    const fakeNext = () => 1;
 
-    
-
-
+    oakTestDash.addStrategy('testStrat', new TestStrat());
+    assertThrowsAsync(async () => await oakTestDash.authenticate('testStrat')(fakeOakCtx, fakeNext));
   },
 });
 
+// Deno.test({
+//   name: "If a session object exists on ctx.state._dashport, authenticate method " + 
+//         "should compare the IDs and invoke next",
+//   fn(): void {
 
-Deno.test({
-  name: "addSerializer method should add a serializer to _serializer",
-  fn(): void {
-    const oakTestDash = new Dashport('oak');
+//   },
+// });
 
-    oakTestDash.addSerializer('1', () => 1);
-    assertEquals()
+// Deno.test({
+//   name: "If a session object does not exist on ctx.state._dashport, or IDs do not match, " + 
+//         "authenticate method should begin the authentication process",
+//   fn(): void {
+
+//   },
+// });
+
+// Deno.test({
+//   name: "authenticate method should .....",
+//   fn(): void {
+
+//   },
+// });
+
+// Deno.test({
+//   name: "addSerializer method should .....",
+//   fn(): void {
+
+//   },
+// });
+
+// Deno.test({
+//   name: "removeSerializer method should .....",
+//   fn(): void {
+
+//   },
+// });
 
 
-  },
-});
+// Deno.test({
+//   name: "addStrategy method should .....",
+//   fn(): void {
+//     const oakTestDash = new Dashport('oak');
+//     const fakeStrat = new TestStrat();
 
-Deno.test({
-  name: "removeSerializer method should remove a serializer on _serializer",
-  fn(): void {
-    const oakTestDash = new Dashport('oak');
+//     assertEquals(oakTestDash[_strategies], {});
+//     oakTestDash.addStrategy('test', fakeStrat);
+//     assertEquals(oakTestDash[_strategies], { test: fakeStrat });
+//   },
+// });
 
-    oakTestDash.removeSerializer('1');
-    assertEquals()
+// Deno.test({
+//   name: "removeStrategy method should .....",
+//   fn(): void {
 
+//   },
+// });
 
-  },
-});
+// Deno.test({
+//   name: "getUserInfo method should .....",
+//   fn(): void {
+
+//   },
+// });
